9876a68e923632c5f9165670a02b35cde27452de
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2015-06-27 20:05:24 +0200

    passing a map to the template

--- main.go
+++ main.go
@@ -3,33 +3,42 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"time"
 )
 
 //Compile templates on start
-var templates = template.Must(template.ParseFiles(
-	"html/header.html",
-	"html/footer.html",
-	"html/index.html",
-	"html/about.html",
-))
-
-//A Page structure
-type Page struct {
-	Title string
-}
+var (
+	templates = template.Must(template.ParseFiles(
+		"html/header.html",
+		"html/footer.html",
+		"html/index.html",
+		"html/about.html",
+	))
+	startTime time.Time
+)
 
 //Display the named template
 func display(w http.ResponseWriter, tmpl string, data interface{}) {
-	templates.ExecuteTemplate(w, tmpl, data)
+	err := templates.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 //The handlers.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	display(w, "index", &Page{Title: "Home"})
+	m := make(map[string]interface{})
+	m["Title"] = "Home"
+	m["Uptime"] = time.Now().Sub(startTime)
+
+	display(w, "index", &m)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	display(w, "about", &Page{Title: "About"})
+	m := make(map[string]interface{})
+	m["Title"] = "About"
+
+	display(w, "about", &m)
 }
 
 func main() {
@@ -38,6 +47,8 @@ func main() {
 	http.HandleFunc("/about", aboutHandler)
 
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+
+	startTime = time.Now()
 
 	//Listen on port 8080
 	http.ListenAndServe(":8080", nil)
